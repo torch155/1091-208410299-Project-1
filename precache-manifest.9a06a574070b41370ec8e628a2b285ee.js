2f9d946fa17adeced5e3fb895ad3b17cf6454215
torch155 <torch885@gmail.com>
2020-10-20 15:07:44 +0800

    Updates

---
+++ precache-manifest.9a06a574070b41370ec8e628a2b285ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9639cfed832252f8bc802154daae021c",
+    "url": "/torch155/1091-208410299-Project-1.git/index.html"
+  },
+  {
+    "revision": "3803e8f3c48a85416f41",
+    "url": "/torch155/1091-208410299-Project-1.git/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f1a91016ec08288d6140",
+    "url": "/torch155/1091-208410299-Project-1.git/static/css/main.424d2976.chunk.css"
+  },
+  {
+    "revision": "3803e8f3c48a85416f41",
+    "url": "/torch155/1091-208410299-Project-1.git/static/js/2.71be9660.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/torch155/1091-208410299-Project-1.git/static/js/2.71be9660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a91016ec08288d6140",
+    "url": "/torch155/1091-208410299-Project-1.git/static/js/main.8dde0156.chunk.js"
+  },
+  {
+    "revision": "7b6fde49746ae5858e0f",
+    "url": "/torch155/1091-208410299-Project-1.git/static/js/runtime-main.08dc2941.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/torch155/1091-208410299-Project-1.git/static/media/SEGA.82e09f78.TTF"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/torch155/1091-208410299-Project-1.git/precache-manifest.9a06a574070b41370ec8e628a2b285ee.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/torch155/1091-208410299-Project-1.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
