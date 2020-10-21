5821082d813aa53b2d6014b485a2c882fd31072f
torch155 <torch885@gmail.com>
2020-10-21 23:49:29 +0800

    Updates

---
+++ precache-manifest.6f53eb025e1d9693cb59d44f75c97956.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e81a050b37cd66d3be4b0bcc81a4f94",
+    "url": "/1091-208410299-Project-1/index.html"
+  },
+  {
+    "revision": "3803e8f3c48a85416f41",
+    "url": "/1091-208410299-Project-1/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "61eb2a1a39eb849e9c3c",
+    "url": "/1091-208410299-Project-1/static/css/main.b7c980af.chunk.css"
+  },
+  {
+    "revision": "3803e8f3c48a85416f41",
+    "url": "/1091-208410299-Project-1/static/js/2.71be9660.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/1091-208410299-Project-1/static/js/2.71be9660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61eb2a1a39eb849e9c3c",
+    "url": "/1091-208410299-Project-1/static/js/main.3ebaf138.chunk.js"
+  },
+  {
+    "revision": "3af8ebc4fe6add5c968d",
+    "url": "/1091-208410299-Project-1/static/js/runtime-main.1bd76cb2.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/1091-208410299-Project-1/static/media/SEGA.82e09f78.TTF"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/torch155/1091-208410299-Project-1/precache-manifest.c86dcced2d5e0549a49d11c9f4af6b86.js"
+  "/1091-208410299-Project-1/precache-manifest.6f53eb025e1d9693cb59d44f75c97956.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/torch155/1091-208410299-Project-1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/1091-208410299-Project-1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
